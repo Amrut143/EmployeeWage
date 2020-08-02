f5256e138316d667d16ae0ad7dfd9481a01173ad
Amrut <amrutpanda7789@gmail.com>
2020-08-02 16:38:47 +0530

    ADDED: Code to add part time employee and calculate wage

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,13 +1,17 @@
-//Constant
+// Constants
 let EMP_RATE_PER_HOUR = 20;
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
 
-//variables
+// Variables
 let isPresent = 1;
+let empCheck = Math.floor(Math.random() * 10) % 3;
 let empHrs = 0, empWage = 0;
-let empCheck = Math.floor(Math.random() * 10) % 2;
 
-if (empCheck == 1)
-    emphrs = 8;
+if (empCheck == IS_PART_TIME)
+    empHrs = 4;
+else if (empCheck == IS_FULL_TIME)
+    empHrs = 8;
 else
     empHrs = 0;
 empWage = empHrs * EMP_RATE_PER_HOUR;
